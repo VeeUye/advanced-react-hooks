50db3f8680cc2e77ef2eb04123a7e49b8fa0c158
Vee Uye <vee.uye@gmail.com>
2023-03-16 11:51:54 +0000

    Advanced React Hooks: useReducer - simulate setState with an object OR function

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -6,14 +6,20 @@ import {useReducer} from 'react'
 
 
 const countReducer = (state, action) => { //action is the value that's passed to the dispqtch function
-  return { ...state, ...action} // returns the state combined with the action
+  // set state with either a function callback or an object
+  return  {
+    ...state,
+    ...(typeof action === 'function' ? action(state) : action),
+  }
 }
   function Counter({initialCount = 0, step = 1}) {
 
     const [state, setState] = useReducer(countReducer, {count: initialCount})
     const { count } = state
 
-  const increment = () => setState({ count: count + step })  //setState is the dispatch function
+    const increment = () =>
+        //setState is the dispatch function
+        setState(currentState => ({count: currentState.count + step}))
 
   return <button onClick={increment}>{count}</button>
 }
