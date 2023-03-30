d0c3c019f5fdf773376febd3d0feb0beb33e1692
Vee Uye <vee.uye@gmail.com>
2023-03-30 13:04:24 +0100

    Advanced React Hooks: useContext - create consumer hook

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -4,14 +4,16 @@
 import * as React from 'react'
 import {createContext, useContext, useState} from 'react'
 
-// 🐨 create your CountContext here with React.createContext
+
 const CountContext = createContext()
 
-// 🐨 create a CountProvider component here that does this:
-//   🐨 get the count state and setCount updater with React.useState
-//   🐨 create a `value` array with count and setCount
-//   🐨 return your context provider with the value assigned to that array and forward all the other props
-//   💰 more specifically, we need the children prop forwarded to the context provider
+const useCount = () => {
+    const context = useContext(CountContext)
+    if(!context) {
+        throw new Error('useCount must be used within the CountProvider')
+    }
+    return context
+}
 
 const CountProvider = (props) => {
     const [count, setCount] = useState(0)
@@ -21,14 +23,12 @@ const CountProvider = (props) => {
 }
 
 function CountDisplay() {
-    const [count] = useContext(CountContext)
-  // 🐨 get the count from useContext with the CountContext
+    const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-    // 🐨 get the setCount from useContext with the CountContext
-    const [,setCount] = useContext(CountContext)
+    const [,setCount] = useCount()
     const increment = () => setCount(c => c + 1)
 
   return <button onClick={increment}>Increment count</button>
